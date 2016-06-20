426bb2afd43a664689f49a577509b0c324b6f216
Ashwin Rao <theonlyrao@gmail.com>
2016-06-20 10:59:18 -0600

    Complete if statement

---
+++ if-statement.js
@@ -0,0 +1,9 @@
+var fruit = "orange";
+
+if (fruit.length > 5) {
+    var result = "The fruit name has more than five characters.";
+} else {
+    var result = "The fruit name has five characters or less.";
+}
+
+console.log(result);
